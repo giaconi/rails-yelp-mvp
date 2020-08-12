8045f8980b5d3f2494e29f325e8d04f9642bc1dc
Iaconi Gheorghe <iaconimgheorghe@gmail.com>
2020-08-12 16:33:15 +0200

    working version no styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,13 +9,16 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    @review.save
+   if @review.save
     redirect_to @restaurant
+   else
+    render :new
+  end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating, :restaurant_id)
   end
 end
